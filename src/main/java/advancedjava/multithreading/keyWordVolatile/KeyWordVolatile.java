336cdc1759c69c47bbbe51764b8cb92be954792a
Dmitry <karpov0910@mail.ru>
2024-02-23 07:39:25 +0300

    Ключевое слово volatile

--- src/main/java/advancedjava/multithreading/keyWordVolatile/KeyWordVolatile.java
+++ src/main/java/advancedjava/multithreading/keyWordVolatile/KeyWordVolatile.java
@@ -1,4 +1,37 @@
 package advancedjava.multithreading.keyWordVolatile;
 
+import java.util.Scanner;
+
+//Синхронизация потоков
 public class KeyWordVolatile {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread.shutdown();
+
+    }
+}
+
+class MyThread extends Thread {
+    private volatile boolean running = true; // Ключевое слово volatile решает проблему когерентности кэшей
+    // необходимо использовать когда несколько потоков обращаются к одной переменной при запуске на многоядерном процесоре
+
+    public void run() {
+        while(running) {
+            System.out.println("hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public  void shutdown() {
+        this.running = false;
+    }
 }
