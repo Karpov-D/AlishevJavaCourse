37ed61ec1e2bf3e9f74aa3851b71ca8948f21b8a
Dmitry <karpov0910@mail.ru>
2024-02-29 06:59:21 +0300

    producer Consumer

---
+++ src/main/java/advancedjava/multithreading/producerConsumer/producerConsumer1.java
@@ -0,0 +1,63 @@
+package advancedjava.multithreading.producerConsumer;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * Реализация патерна с помощью потокобезопастной очереди
+ */
+public class producerConsumer1 {
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+
+    private static void produce() throws InterruptedException {
+        Random random = new Random();
+
+        while (true) {
+            queue.put(random.nextInt(100)); //Метод  ждет если очередь заполнена
+        }
+    }
+
+    private static void consumer() throws InterruptedException {
+        Random random = new Random();
+
+        while (true) {
+            Thread.sleep(100);
+
+            if (random.nextInt(10) == 5) {
+                System.out.println(queue.take());
+                System.out.println("Queue size is " + queue.size());
+            }
+        }
+    }
+}
